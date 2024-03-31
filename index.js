7c9ed51c4dd4f6504badaa188ba4fdb46c11cb72
tabinda ajaz <159224662+Tabinaajaz@users.noreply.github.com>
2024-03-31 19:55:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//function
+function transportName(bike, car, bus, train) {
+    if (bike === void 0) { bike = 1; }
+    if (car === void 0) { car = 3; }
+    if (bus === void 0) { bus = 2; }
+    if (train === void 0) { train = 1; }
+    var number = bike + car + bus + train;
+    return number;
+}
+console.log(transportName(2, 3, 1, 4));
+function furitchaat(Apple, banana, Melo) {
+    if (Apple === void 0) { Apple = 1; }
+    if (banana === void 0) { banana = 4; }
+    if (Melo === void 0) { Melo = 1; }
+    var making = Apple + banana + Melo;
+    return making;
+}
+console.log(furitchaat(1, 5, 1));
---
+++ index.ts
@@ -0,0 +1,12 @@
+
+//function
+function transportName(bike=1,car=3,bus=2,train=1){
+    let number = bike+car+bus+train
+    return number
+}
+console.log(transportName(2,3,1,4));
+function furitchaat(Apple=1,banana=4,Melo=1,){
+    let making= Apple+banana+Melo
+    return making
+}
+console.log(furitchaat(1,5,1))
